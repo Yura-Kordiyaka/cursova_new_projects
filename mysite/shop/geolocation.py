7a1f6d1889c68d823a0bf12be01f419e8cdab966
Yura162 <yurakordiyaka2004@gmail.com>
2024-03-30 16:19:58 +0200

    first commit

---
+++ mysite/shop/geolocation.py
@@ -0,0 +1,16 @@
+import googlemaps
+from django.conf import settings
+
+
+def get_coordinates(address: str):
+    gmaps = googlemaps.Client(key=settings.GOOGLE_API_KEY)
+
+    geocode_result = gmaps.geocode(address)
+
+    if geocode_result and len(geocode_result) > 0:
+        location = geocode_result[0]['geometry']['location']
+        latitude = location['lat']
+        longitude = location['lng']
+        return latitude, longitude
+    else:
+        pass
